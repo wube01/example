c78138ec28eeb1aae7575daa3bcd5281e984d75f
Ülari Ainjärv <ylari.ainjarv@gmail.com>
2025-12-05 10:38:06 +0200

    Service and location implemented

--- lib/home/welcome.dart
+++ lib/home/welcome.dart
@@ -1,40 +1,57 @@
 import 'package:example/common/header.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart';
 
 class Welcome extends StatefulWidget {
   const Welcome({super.key});
 
   @override
   State<StatefulWidget> createState() => _WelcomeState();
-  
 }
 
 class _WelcomeState extends State<Welcome> {
-
   @override
   void initState() {
     super.initState();
-    _load();
+    _getLocation().then((position) {
+      debugPrint(position.toString());
+      _load(position.latitude.toString(), position.longitude.toString());
+    });
   }
 
-  void _load() async {
-    await http.get(Uri.https('api.met.no',
-    'weatherapi/locationforecast/2.0/compact?lat=59.42709985708767&lon=24.536038897750476'),
-    headers: {
-      'User-Agent': 'minu eriti vinge app',
-      'Access-Control-Allow-Origin': 'true'
-    }).then((response) {
-      debugPrint(response.body);
-    });
+  void _load(String lat, String lon) async {
+    await http
+        .get(
+          Uri.https('api.met.no', 'weatherapi/locationforecast/2.0/compact', {
+            'lat': lat,
+            'lon': lon,
+          }),
+          headers: {'User-Agent': 'MyApp/1.0 (ylari.ainjarv@gmail.com)'},
+        )
+        .then((response) {
+          debugPrint(response.body);
+        });
+  }
+
+  Future<Position> _getLocation() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception('Location not available');
+    }
+    return await GeolocatorPlatform.instance.getCurrentPosition();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Header(title: 'Avaleht'),
-      body: Center(child: Image.asset('assets/ylari.jpeg', width: 90, height: 90,),),
+      body: Center(
+        child: Image.asset('assets/ylari.jpeg', width: 90, height: 90),
+      ),
     );
   }
-  
 }
